b6f96ec19e484525c7a0b84755d88648837b458d
chacha86 <taejincha0619@gmail.com>
2025-09-25 16:40:21 +0900

    work

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { fetchApi } from "@/lib/client";
 import Link from "next/link";
 
 export default function ClientLayout({
@@ -6,6 +7,19 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const logout = () => {
+    confirm("로그아웃 하시겠습니까?") &&
+      fetchApi("/api/v1/members/logout", {
+        method: "DELETE",
+      })
+        .then((data) => {
+          alert(data.msg);
+        })
+        .catch((rsData) => {
+          alert(rsData.msg);
+        });
+  };
+
   return (
     <>
       <header>
@@ -13,13 +27,7 @@ export default function ClientLayout({
           <Link href="/">메인</Link>
           <Link href="/posts">글 목록</Link>
           <Link href="/members/login">로그인</Link>
-          <button
-            onClick={() => {
-              console.log("로그아웃");
-            }}
-          >
-            로그아웃
-          </button>
+          <button onClick={logout}>로그아웃</button>
         </nav>
       </header>
       <main className="flex-1 flex flex-col justify-center items-center">
