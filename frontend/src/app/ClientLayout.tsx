f5fe62a8fa78cf3b1fa07bd12c4a6cbf2ce1fc95
chacha86 <taejincha0619@gmail.com>
2025-09-26 14:14:55 +0900

    work

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,41 +1,8 @@
 "use client";
-import { fetchApi } from "@/lib/client";
-import { FetchCallbacks } from "@/type/client";
-import { MemberDto } from "@/type/member";
+import { useAuth, AuthContext } from "@/global/auth/hooks/useAuth";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { createContext, useEffect, useState } from "react";
-
-const AuthContext = createContext<ReturnType<typeof useAuth> | null>(null);
-
-function useAuth() {
-  const [loginMember, setLoginMember] = useState<MemberDto | null>(null);
-  const getLoginMember = (callbacks: FetchCallbacks) => {
-    fetchApi("/api/v1/members/me")
-      .then((data) => {
-        setLoginMember(data.data.memberDto);
-        callbacks.onSuccess?.(data);
-      })
-      .catch((err) => {
-        callbacks.onError?.(err);
-      });
-  };
-  const logout = (callbacks: FetchCallbacks) => {
-    confirm("로그아웃 하시겠습니까?") &&
-      fetchApi("/api/v1/members/logout", {
-        method: "DELETE",
-      })
-        .then((data) => {
-          setLoginMember(null);
-          callbacks.onSuccess?.(data);
-        })
-        .catch((rsData) => {
-          callbacks.onError?.(rsData.msg);
-        });
-  };
-
-  return { loginMember, getLoginMember, logout };
-}
+import { useEffect } from "react";
 
 export default function ClientLayout({
   children,
---
+++ frontend/src/global/auth/hooks/useAuth.ts
@@ -0,0 +1,37 @@
+import { fetchApi } from "@/lib/client";
+import { FetchCallbacks } from "@/type/client";
+import { MemberDto } from "@/type/member";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext<ReturnType<typeof useAuth> | null>(
+  null
+);
+
+export function useAuth() {
+  const [loginMember, setLoginMember] = useState<MemberDto | null>(null);
+  const getLoginMember = (callbacks: FetchCallbacks) => {
+    fetchApi("/api/v1/members/me")
+      .then((data) => {
+        setLoginMember(data.data.memberDto);
+        callbacks.onSuccess?.(data);
+      })
+      .catch((err) => {
+        callbacks.onError?.(err);
+      });
+  };
+  const logout = (callbacks: FetchCallbacks) => {
+    confirm("로그아웃 하시겠습니까?") &&
+      fetchApi("/api/v1/members/logout", {
+        method: "DELETE",
+      })
+        .then((data) => {
+          setLoginMember(null);
+          callbacks.onSuccess?.(data);
+        })
+        .catch((rsData) => {
+          callbacks.onError?.(rsData.msg);
+        });
+  };
+
+  return { loginMember, getLoginMember, logout };
+}
